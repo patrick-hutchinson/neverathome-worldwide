fa29b29e11881d9a0e72c1612332aa7b32f426c9
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-08-31 11:02:26 +0200

    Expose Sanity preview environment flag

--- app/src/pages/_app.js
+++ app/src/pages/_app.js
@@ -48,6 +48,10 @@ const cityListTransitionVariants = {
 // Updated Packages
 
 const isProduction = process.env.VERCEL_ENV === "production";
+export const isSanityPreviewEnvironment = process.env.NEXT_PUBLIC_VERCEL_ENV !== "production";
+
+console.log(isProduction, "isProduction");
+console.log(isSanityPreviewEnvironment, "isSanityPreviewEnvironment");
 // const isProduction = true;
 
 const routeMarqueeLabels = {
