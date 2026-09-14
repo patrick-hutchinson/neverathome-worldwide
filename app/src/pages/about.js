0c412c0a798ac1d48f3e0354fbb82b1dd22a8a76
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-09-14 23:31:16 +0200

    Refine about page and link styling

--- app/src/pages/about.js
+++ app/src/pages/about.js
@@ -39,12 +39,6 @@ const AboutPage = ({ aboutPage }) => {
               The Team
             </h3>
 
-            <Text
-              text="Hier kommt noch ein kleiner Text hin, ich weiss noch nicht ganz was."
-              typo="h6 compensate"
-              className={styles.text}
-            />
-
             <div className={styles.teamPhotos}>
               {aboutPage.team.map((teamMember, index) => {
                 return (
